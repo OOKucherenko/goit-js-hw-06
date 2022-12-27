e6e9e6c7d2d28be00a9ad453e0b8b4055457d18c
OOKucherenko <okld52dec@gmail.com>
2022-12-27 09:33:01 +0200

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+ 
+
+
+const bodyEl = document.querySelector(`body`);
+const colorSpan = document.querySelector(`.color`);
+const buttonColor = document.querySelector(`.change-color`);   
+ 
+buttonColor.addEventListener("click", changecolor);
+
+
+function changecolor() {
+  colorSpan.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  
+}
+
+ 
+
+
